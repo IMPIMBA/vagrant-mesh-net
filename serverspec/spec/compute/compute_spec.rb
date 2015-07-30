1bffdeb43d6dd62a34d043bb6f490527e7828e10
Karner,Andreas <andreas.karner@imp.ac.at>
2015-07-30 16:45:28 +0200

    Removed old serverspec

--- serverspec/spec/compute/compute_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe process("sge_execd") do
-  it { should be_running }
-end
-
-describe port(803) do
-  it { should be_listening}
-end
--- serverspec/spec/login/login_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe command('echo $PATH') do
-  its(:stdout) { should contain('/vagrant/UGE/bin/lx-amd64') }
-end
