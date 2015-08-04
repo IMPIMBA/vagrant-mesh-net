89949b084dc55cfca69366eae70008b2f8060bb0
Karner,Andreas <andreas.karner@imp.ac.at>
2015-08-04 15:44:11 +0200

    Added Rakefile for Creating and Testing machine, imporved check script

--- packer/serverspec/test-box.sh
+++ packer/serverspec/test-box.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-create_vagrantfile_linux() {
+create_vagrantfile() {
 vagrant plugin list | (grep vagrant-serverspec > /dev/null) || vagrant plugin install vagrant-serverspec
 cat << EOF > $tmp_path/Vagrantfile
 Vagrant.configure('2') do |config|
@@ -13,8 +13,9 @@ end
 EOF
 }
 
-box_path="../centos6-x64.box"
-test_src_path="$(pwd)/test/*_spec.rb"
+base_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+box_path="$base_path/../centos6-x64.box"
+test_src_path="$base_path/test/*_spec.rb"
 
 box_filename=$(basename "${box_path}")
 box_name=${box_filename%.*}
@@ -23,24 +24,28 @@ tmp_path=/tmp/boxtest
 rm -rf ${tmp_path}
 
 export VAGRANT_CWD="/"
-vagrant box remove ${box_name}
-vagrant box add ${box_name} ${box_path}
+vagrant box remove ${box_name} &> /dev/null
+vagrant box add ${box_name} ${box_path} &> /dev/null
 
 mkdir -p ${tmp_path}
 
-pushd ${tmp_path}
-create_vagrantfile_linux
+pushd ${tmp_path} &> /dev/null
+create_vagrantfile
 
 export VAGRANT_CWD="$(pwd)"
 vagrant up
 if (($? > 0)); then
-  echo "There was an error!"
+  errorcode=1
+else
+  errorcode=0
 fi
 sleep 10
-vagrant destroy -f
-popd
+vagrant destroy -f &> /dev/null
+popd &> /dev/null
 
 export VAGRANT_CWD="/"
-vagrant box remove ${box_name}
+vagrant box remove ${box_name} &> /dev/null
 unset VAGRANT_CWD
 rm -rf ${tmp_path}
+
+exit $errorcode
